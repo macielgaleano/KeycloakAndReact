14fc555c396a1ac06c4c83eaabdaff387c3c8ac7
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-12-26 09:02:21 -0300

    no me acuerdo

--- keycloackreact/src/App.js
+++ keycloackreact/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Secured from "./components/Secured";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <ul>
+          <li>
+            <Link to="/">public component</Link>
+          </li>
+          <li>
+            <Link to="/secured">secured component</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/secured" component={Secured} />
+      </div>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
---
+++ keycloackreact/src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Welcome = () => {
+  return <h1>Welcome to the jungle, because this a public component</h1>;
+};
+
+export default Welcome;
